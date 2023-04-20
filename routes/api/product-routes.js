a6b041ad0d30257776096b8547079a6e4c879acc
Michrira <Michaelchristianrader@gmail.com>
2023-04-19 19:20:18 -0700

    changed product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,15 +7,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then((ProductData) => {
+    res.status(200).json(ProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id,{
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then((productData) => {
+    res.status(200).json(productData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
-// create new product
+// create new product - DO NOT CHANGE THIS FOR INSOMNIA
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -47,7 +82,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// update product SEE OBJECT in LINE 22
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -91,6 +126,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.status(200).json({message: "Product deleted successfully"});
+    })
+    .catch((err) => res.json(err));
 });
+
 
 module.exports = router;
